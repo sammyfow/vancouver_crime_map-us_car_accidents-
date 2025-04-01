93bee9613f24e49411ae9c54b98acd2ab1be184f
sammyfow <156115889+sammyfow@users.noreply.github.com>
2025-04-01 15:46:41 +0000

    updated

--- HA_regression.py
+++ HA_regression.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import seaborn as sns
 import sqlite3
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -37,4 +38,36 @@ def get_lin_regression(params):
 
     print(f'Coefficient of determination: {score}\nPearson coefficient: {pc}\nP value: {pv}\nEquation: y = ({model.coef_[0]})x + ({model.intercept_})')
 
-    
+def get_multiple_regression(params):
+    df = get_data()
+
+    model = LinearRegression()
+    model.fit(df[params], df['logcount'])
+    score = model.score(df[params], df['logcount'])
+    values = model.get_params()
+
+    print(f'Coefficient of determination: {score}\nEquation: ln(count) = ({model.coef_[0]})population + ({model.coef_[1]})ln(price) + ({model.intercept_})')
+
+def plot_prediction():
+    df = get_data()
+
+    xs = df[['population', 'logprice']]
+    ys = df['logcount']
+
+    model = LinearRegression()
+    model.fit(xs, ys)
+    ypred = model.predict(xs)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+
+    sns.kdeplot(ys, ax=ax1)
+    sns.kdeplot(ypred, ax=ax2)
+
+    ax1.set_xlim(-2.0, 12.0)
+    ax2.set_xlim(-2.0, 12.0)
+    ax2.set_ylabel('')
+    ax2.set_xlabel('predicted count')
+
+    plt.savefig('distribution.png')
